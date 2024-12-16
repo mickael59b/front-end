2649781eb4a4560e1f321cd543a734e30378da1d
mickael59b <mboutte621@gmail.com>
2024-12-16 08:58:44 +0100

    update bug build

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      // Force l'alias pour Bootstrap
-      'bootstrap': '/node_modules/bootstrap/dist/css/bootstrap.min.css',
-    },
-  },
-  build: {
-    // Assurez-vous que Vite ne rencontre pas de problème avec certains modules
-    commonjsOptions: {
-      include: /node_modules/,
-    },
-  },
-})
+});
+
 
