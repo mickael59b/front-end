d927e8f6822d7cdd6e39b0cf5f3a4fc9925828c0
mickael59b <mboutte621@gmail.com>
2024-12-16 11:06:20 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Gardez '/' pour le déploiement à la racine
+  base: '/', // Remplacez par le chemin si nécessaire (par ex. '/app/').
+  build: {
+    outDir: 'dist', // Dossier où les fichiers générés sont stockés.
+    assetsDir: 'assets', // Sous-dossier pour les fichiers statiques.
+  },
 });
-
-
